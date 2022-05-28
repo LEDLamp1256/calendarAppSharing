ef285ceefd2d64007032f8487e1e83df54b2910a
LEDLamp1256 <dylanlee0613@gmail.com>
2022-05-28 10:01:26 -0700

    5 21

--- event.py
+++ event.py
@@ -1,4 +1,9 @@
-class Event:
-    def __init__(self, name: str, time: str):
+import tkinter as tk
+from tkinter import ttk
+
+
+class Event(ttk.Frame):
+    def __init__(self, name: str, time: str, superwidget : tk.Widget, fullscreen = True):
+        super(Event, self).__init__(superwidget)
         self.name = name
         self.time = time
--- main.py
+++ main.py
@@ -13,14 +13,20 @@ frame.config()
 frame.grid()
 ttk.Label(frame, text = "Month").grid(column = 10, row = 10)
 
+daysofmonth = tk.Frame(frame, padx=10, pady=10)
 daysframe = ttk.Frame(frame, padding=10)
 daysframe.grid(column = 10, row = 20)
 daysofweek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
 for i in range(7):
-    ttk.Label(daysframe, text = daysofweek[i]).grid(column = i, row = 20)
+    tk.Label(daysofmonth, text = daysofweek[i], width = 5, height = 2).grid(column = i, row = 0, padx = 2, pady = 2)
 #ttk.Button(frame, text = "Exit button", command = root.destroy).grid(column = 10, row = 20)
 
-daysofmonth = tk.Frame(frame, padx=10, pady=10)
+dayViewFrame = ttk.Frame(frame, padding=10)
+dayViewFrame.grid(column = 100 , row = 0)
+def editDay(event, dayNumber):
+    tk.Label(dayViewFrame, text = event).grid(column = 100, row = 0, padx = 2, pady = 2)
+
+#daysofmonth = tk.Frame(frame, padx=10, pady=10)
 daysofmonth.grid(column=10, row=30)
 daysofmonth.config(background = "grey")
 cal = calendar.TextCalendar(calendar.MONDAY)
@@ -31,6 +37,6 @@ for idx, i in enumerate(cal.itermonthdays(2022, 5)):
         pass
     else:
         lbl = tk.Label(daysofmonth, text=i, width = 5, height = 5)
-        lbl.grid(column=idx%7, row=idx//7, padx = 2, pady = 2)
-        lbl.bind("<Button-1>", lambda e, ("Hi"))
+        lbl.grid(column=idx%7, row=idx//7 + 10, padx = 2, pady = 2)
+        lbl.bind("<Button-1>", lambda e: editDay(e, i))
 root.mainloop()
