c6df2bcf12627ceddbe9ad0196ee6ec2468227f9
Dylan <dylanlee0613@gmail.com>
2022-06-18 10:00:25 -0700

    6/18

--- main.py
+++ main.py
@@ -29,16 +29,36 @@ for i in range(7):
 
 dayViewFrame = ttk.Frame(frame, padding=10)
 dayViewFrame.grid(column = 100 , row = 30)
+innerFrameEvents = ttk.Frame(dayViewFrame, padding = 10)
+innerFrameEvents.grid(column =0 , row =0)
+innerFrameCreation = ttk.Frame(dayViewFrame, padding = 10)
+innerFrameCreation.grid(column = 0, row = 1)
 placeholder1 = Event("placeholder1", "12:00", dayViewFrame)
 placeholder2 = Event("placeholder2", "13:00", dayViewFrame)
 placeholder3 = Event("placeholder3", "14:00", dayViewFrame)
 eventList = [placeholder1, placeholder2, placeholder3]
+
+def addEvent(time, name):
+    addEvent = Event(name, time, innerFrameEvents)
+    addEvent.grid()
+
 def editDay(event, date:datetime.datetime):
+    innerFrameEvents.winfo_children()
+    for child in innerFrameEvents.winfo_children():
+        child.grid_remove()
     eventList = dayStorage.get(date, [])
     print(eventList)
     for idx, i in enumerate(eventList):
         i.grid(column = 10, row = 0 + idx, sticky = NSEW)
         print(i)
+    timeEntry = tk.StringVar()
+    timeEntry.set("00:00")
+    ttk.Entry(innerFrameCreation, textvariable=timeEntry).grid(column = 0, row = 0)
+    nameEntry = tk.StringVar()
+    nameEntry.set("placeholder name")
+    ttk.Entry(innerFrameCreation, textvariable=nameEntry).grid(column = 1, row = 0)
+    ttk.Button(innerFrameCreation, text = "Add Event", command = lambda: addEvent(timeEntry.get(), nameEntry.get())).grid(column = 2, row = 0)
+
 
     #tk.Label(dayViewFrame, text = event).grid(column = 100, row = 0, padx = 2, pady = 2)
 
@@ -57,6 +77,7 @@ for idx, day in enumerate(cal.itermonthdays(year, month)):
 
         lbl = tk.Label(daysofmonth, text=day, width = 5, height = 5)
         lbl.grid(column=idx%7, row=idx//7 + 10, padx = 2, pady = 2)
-        lbl.bind("<Button-1>", lambda e: editDay(e, datetime.datetime(year, month, day)))
+        datetimestorage = datetime.datetime(year, month, day)
+        lbl.bind("<Button-1>", lambda e: editDay(e, datetimestorage))
 print(day)
 root.mainloop()
